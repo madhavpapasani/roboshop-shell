8d06269755d6fa16759fc8e91830f1dbe3f4c898
madha <madhavpapasani@rocketmail.com>
2023-05-28 19:31:51 +0530

    Redis changes

--- redis.sh
+++ redis.sh
@@ -5,7 +5,7 @@ yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 echo -e "\e[31mInstall redis\e[0m"
 yum install redis -y &>>/tmp/roboshop.log
 echo -e "\e[31mUpdate Redis Listen Address \e[0m"
-sed -i 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 echo -e "\e[31mStart redis service\e[0m"
 systemctl enable redis &>>/tmp/roboshop.log
 systemctl restart redis &>>/tmp/roboshop.log
