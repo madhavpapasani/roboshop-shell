6915902f606f2f6763313efdce88fdd9ac29967c
madha <madhavpapasani@rocketmail.com>
2023-05-28 22:21:15 +0530

    shipping script changes

--- mongodb.sh
+++ mongodb.sh
@@ -6,4 +6,4 @@ echo -e "\e[32mUpdate mongodb listen address\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo -e "\e[33mStarting mongodb service\e[0m"
 systemctl enable mongod &>>/tmp/mongodb.log
-systemctl start mongod &>>/tmp/mongodb.log
+systemctl restart mongod &>>/tmp/mongodb.log
