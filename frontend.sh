09c4e323416245b6d890c601528f1656085bf7b8
madha <madhavpapasani@rocketmail.com>
2023-05-28 17:44:58 +0530

    Roboshop ngnix shell script

--- frontend.sh
+++ frontend.sh
@@ -7,9 +7,8 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 echo -e "\e[34mExtracting frontend content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
-#we need to config config file
-echo -e "\e[34mUpdate \e[0m"
-
+echo -e "\e[34mUpdate Frontend Configuration\e[0m"
+cp robshop.config /etc/nginx/default.d/roboshop.conf
 echo -e "\e[35mStarting Nginx Service\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
 systemctl restart nginx &>>/tmp/roboshop.log
