90bdd58a5d6e3f8d1570f12442f00121a0a7f05f
madha <madhavpapasani@rocketmail.com>
2023-05-29 17:25:13 +0530

    script changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,29 +1,29 @@
 echo -e "\e[31mConfiguring Nodejs repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo -e "\e[31mInstall Nodejs\e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log
+yum install nodejs -y
 echo -e "\e[31mAdd application user\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+useradd roboshop
 echo -e "\e[31mCreate application directory\e[0m"
-rm -ef /app &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
+rm -ef /app
+mkdir /app
 echo -e "\e[31mDownload application content\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
-cd /app &>>/tmp/roboshop.log
+cd /app
 echo -e "\e[31mExtract application content\e[0m"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip
 cd /app
 echo -e "\e[31mInstall Nodejs dependencies\e[0m"
-npm install &>>/tmp/roboshop.log
+npm install
 echo -e "\e[31mSetup systemD service\e[0m"
-cp /home/centos/roboshop-shell/catalogue.service vim /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
-echo -e "\e[31mStart Catalogue service\e[0m" &>>/tmp/roboshop.log
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/catalogue.service vim /etc/systemd/system/catalogue.service
+echo -e "\e[31mStart Catalogue service\e[0m"
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
 echo -e "\e[31mCopy mongodb repo\e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[31mInstall mongodb client\e[0m"
-yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+yum install mongodb-org-shell -y
 echo -e "\e[31mLoad schema\e[0m"
-mongo --host mongodb-dev.madhavdevops.store </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mongodb-dev.madhavdevops.store </app/schema/catalogue.js
--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,9 @@
 echo -e "\e[31mCopy Mondodb file\e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/mongodb.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[32mInstall mongodb\e[0m"
-yum install mongodb-org -y &>>/tmp/mongodb.log
+yum install mongodb-org -y
 echo -e "\e[32mUpdate mongodb listen address\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo -e "\e[33mStarting mongodb service\e[0m"
-systemctl enable mongod &>>/tmp/mongodb.log
-systemctl restart mongod &>>/tmp/mongodb.log
+systemctl enable mongod
+systemctl restart mongod
