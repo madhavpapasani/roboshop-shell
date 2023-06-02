e674490892a6fd8f2c011c5f0bf033a053da819f
madha <madhavpapasani@rocketmail.com>
2023-06-02 18:02:24 +0530

    script changes

--- common.sh
+++ common.sh
@@ -27,3 +27,12 @@ nodejs() {
   systemctl enable $component &>>$log_file
   systemctl restart $component &>>$log_file
 }
+
+mongo_schema_setup(){
+  echo -e "${color}Copy mongodb repo${nocolor}"
+  cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
+  echo -e "${color}Install mongodb client${nocolor}"
+  yum install mongodb-org-shell -y &>>$log_file
+  echo -e "${color}Load schema${nocolor}" &>>$log_file
+  mongo --host mongodb-dev.madhavdevops.store <${app_path}/schema/$component.js &>>$log_file
+}
